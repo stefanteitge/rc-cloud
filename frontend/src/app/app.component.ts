2e4c387ecb64f214592eb5cc78592715d1b467c2
Stefan Teitge <stefan.teitge@gmail.com>
2025-04-05 20:31:29 +0200

    Migrated to Bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
--- frontend/src/app/pages/races/components/race-meeting-list/race-meeting-list.component.ts
+++ frontend/src/app/pages/races/components/race-meeting-list/race-meeting-list.component.ts
@@ -1,21 +1,11 @@
 import {Component, OnInit, signal} from '@angular/core';
 import { RaceMeetingRepository} from '../../../../shared/race-meeting/repositories/race-meeting.repository';
-import {MatChip} from '@angular/material/chips';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef,
-  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
-  MatTable
-} from '@angular/material/table';
 import {UpcomingDate} from '../../../../shared/race-meeting/domain/upcoming-meetings';
 import {NgbAlert} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-race-meeting-list',
-  imports: [MatChip, MatTable, MatCell, MatHeaderCell, MatColumnDef, MatHeaderRow, MatHeaderCellDef, MatCellDef, MatHeaderRowDef, MatRow, MatRowDef, NgbAlert],
+  imports: [NgbAlert],
   templateUrl: './race-meeting-list.component.html',
   styleUrl: './race-meeting-list.component.scss',
   providers: [RaceMeetingRepository]
