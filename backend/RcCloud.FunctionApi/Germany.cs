31ab098b1a518301424b568d949c9753814c741a
Stefan Teitge <stefan.teitge@gmail.com>
2025-04-07 21:52:40 +0200

    Minor improvements

--- backend/RcCloud.FunctionApi/Germany.cs
+++ backend/RcCloud.FunctionApi/Germany.cs
@@ -2,23 +2,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using RcCloud.DateScraper.Domain.Races;
+using RcCloud.DateScraper.Domain.Series;
 
 namespace RcCloud.FunctionApi
 {
-    public class Races
+    public class Germany
     {
-        private readonly ILogger<Races> _logger;
+        private readonly ILogger<Germany> _logger;
 
-        public Races(ILogger<Races> logger)
+        public Germany(ILogger<Germany> logger)
         {
             _logger = logger;
         }
 
-        [Function("Races")]
+        [Function("germany")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to racing!");
+            return new OkObjectResult(new RaceMeeting([SeriesReference.None], SeasonReference.Current, new DateOnly(2025, 4, 7), "here", "de", "Foo race", [], null, null));
         }
     }
 }
