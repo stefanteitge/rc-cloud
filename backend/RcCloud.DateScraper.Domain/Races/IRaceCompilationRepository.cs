2944007e452938ee23e894337fa5c8cd9f6a527d
Stefan Teitge <stefan.teitge@gmail.com>
2026-02-04 07:04:08 +0100

    Missing files

---
+++ backend/RcCloud.DateScraper.Domain/Races/IRaceCompilationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RcCloud.DateScraper.Domain.Races;
+
+public interface IRaceCompilationRepository
+{
+    Task<RaceCompilation?> Load(string compilation, string source);
+
+    Task<bool> Store(List<RaceMeeting> races, string compilation, string source);
+}
---
+++ backend/RcCloud.DateScraper.Domain/Races/RaceCompilation.cs
@@ -0,0 +1,20 @@
+﻿namespace RcCloud.DateScraper.Domain.Races;
+
+public class RaceCompilation
+{
+    public RaceCompilation(string compilation, string source, List<RaceMeeting> races, DateTimeOffset lastUpdate)
+    {
+        Compilation = compilation;
+        Races = races;
+        LastUpdate = lastUpdate.ToString();
+        Source = source;
+    }
+    
+    public string Compilation { get; set; }
+
+    public List<RaceMeeting> Races { get; set; } = [];
+    
+    public string LastUpdate { get; set; }
+
+    public string Source { get; set; }
+}
